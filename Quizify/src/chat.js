2c06394be8efb283167e577ffed1ed901a06b310
Ilinats <107075825+Ilinats@users.noreply.github.com>
2024-03-16 06:12:32 +0200

    index style

--- Quizify/src/chat.js
+++ Quizify/src/chat.js
@@ -71,7 +71,7 @@ const styles = StyleSheet.create({
   container: {
 		marginTop: 40,
 		marginVertical: -10,
-		marginHorizontal: 100,
+		marginHorizontal: 30,
 		alignItems: 'center',
 		backgroundColor: '#ff6262',
 		padding: 15,
